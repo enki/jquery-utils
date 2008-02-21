ffcad468fd417795f0b2b14e7c01424a0afc48e8
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-02-21 20:30:24 +0000

    - jquery.cycle: changed namespace to "cycle" only - fixed variables scope for multiple instances

--- jquery.cycle.js
+++ jquery.cycle.js
@@ -1,30 +1,50 @@
-(function($) {
-    var index    = 0;
-    var img      = new Image();
-    var images   = [];
-    var element  = null;
+/*
+  jQuery cycle - 0.2
+  (c) 2008 ~ Maxime Haineault (haineault@gmail.com)
+  http://haineault.com   
 
-    var callback = function(image) {
-                if ($.browser.mozilla) window.scrollTo(window.pageXOffset, window.pageYOffset); // scrollfix
-        $(element).hide().css({backgroundImage:'url("'+ images[index] +'")'});
+  MIT License (http://www.opensource.org/licenses/mit-license.php)
+
+  changelog
+  =========
+  -- 0.2 --
+  - changed namespace to cycle
+  - now allowing custom callback functions
+  - fixed variable scope bug with multiple instances
+  - added license
+*/
+
+(function($) {
+
+    function def_callback(element, image) {
+        if ($.browser.mozilla) window.scrollTo(window.pageXOffset, window.pageYOffset); // scrollfix
+        $(element).hide().css({backgroundImage:'url("'+ image +'")'});
         $(element).fadeIn('slow');
-    };
+    }
 
     function shuffle (b) {
         for(var j, x, i = b.length; i; j = parseInt(Math.random() * i, 10), x = b[--i], b[i] = b[j], b[j] = x);
         return b;
     }
 
-    function cycle() {
-        callback.apply(this, [element, images[index]]);
-        if (index < images.length-1) index++;
-        else index = 0;
-    }
-
     $.extend($.fn, {
-        cycleImages: function (imgs, delay, cb) {
-            images   = shuffle(imgs), element = $(this).get(0), callback =  cb || callback;
+        cycle: function (imgs, delay, cb) {
+            var index    = 0;
+            var img      = new Image();
+            var images   = [];
+            var element  = null;
+            var images   = shuffle(imgs)
+            var element  = $(this).get(0)
+            var callback = cb || def_callback;
+
+            function cycle() {
+                callback.apply(this, [element, images[index]]);
+                if (index < images.length-1) index++;
+                else index = 0;
+            }
+
             for (id in images) { img.src = images[id]; } // preload
+
             setInterval(cycle, delay);
             cycle();
         }
