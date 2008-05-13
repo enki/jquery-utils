83c59ed9f65460a6311b16f5323072cdfb14d0d0
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-05-13 14:32:29 +0000

    - added test suite - added basic stings tests for jquery.strings.js - added basic extensible conversion support for jquery.strings.js

--- jquery.strings.js
+++ jquery.strings.js
@@ -11,82 +11,100 @@
     named arguments
     ~~~~~~~~~~~~~~~
 
+    extensible conversions
+    ~~~~~~~~~~~~~~~~~~~~~~
+    jQuery.extend(jQuery.strConversion, 
+        {'u': function(arg){ return arg.toUpperCase(); }
+    });
 */
 
 (function(){
-    var formatToken = function(token) {
-        return token;
+
+    var conversion = {
+        // string
+        's': function(arg) {
+            return (typeof(arg) == 'string')? arg: arg.join('');
+        },
+        'd': function(arg) {
+            return parseInt(arg);
+        }
+    };
+
+    var formatToken = function(token, arg) {
+        console.log('Format: %s -> %o', token, arg);
+        var match  = token.split(':');
+        var token  = match[0];
+        var format = (match[1] && conversion[match[1]])? match[1]: 's';
+        return conversion[format](arg);
     };
 
     var format =  function(str, args) {
         var end        = 0;
         var start      = 0;
+        var match      = false;
         var buffer     = [];
         var token      = '';
         var args       = args || {};
         var getFormat  = function(str) { return 'd'; }; // TODO
-
-        for(index in str.split('')) {
+        var star       = str.split('');
+        for(index in str) {
             start = parseInt(index);
             if (str[start] == '{' && str[start+1] !='{') {
                 end   = str.indexOf('}', start);
                 token = str.slice(start+1, end);
-
-                if (!args[token]) buffer.push('{'+token+'}');
-                else buffer.push(format(args[token], formatToken(args[token])));
+                match = token.match(/^\w+/)[0] || token;
+                if (!args[match]) buffer.push('{'+token+'}');
+                else buffer.push(formatToken(token, args[match]));
             }
             else if (start > end || buffer.length < 1)  buffer.push(str[start]);
         }
-        console.log('Buffer: %s', buffer.join(''));
+        console.log('Formatted: %s -> %a -> %s', str, buffer, buffer.join(''));
         return buffer.join('');
     };
 
-    $.extend({
-        format: format,
+    var sprintf = function() {
         // This code is in the public domain. Feel free to link back to http://jan.moesen.nu/
         // ---
         // Changes made by Maxime Haineault (2007):
         // - The function is now extended to Strings
         // - The function now accept arrays as arguments, much easier to handle
-        sprintf: function() {
-            if (!arguments || !RegExp) return;
-            var str = this;
-            var re  = /([^%]*)%('.|0|\x20)?(-)?(\d+)?(\.\d+)?(%|b|c|d|u|f|o|s|x|X)(.*)/; //'
-            var a   = b = [], i = 0, numMatches = 0;
+        if (!arguments || !RegExp) return;
+        var str = this;
+        var re  = /([^%]*)%('.|0|\x20)?(-)?(\d+)?(\.\d+)?(%|b|c|d|u|f|o|s|x|X)(.*)/; //'
+        var a   = b = [], i = 0, numMatches = 0;
 
-            if (typeof arguments[0] == 'object') arguments = $A(arguments[0]);
-            while (a = re.exec(str)) {
-                var leftpart   = a[1], pPad  = a[2], pJustify  = a[3], pMinLength = a[4];
-                var pPrecision = a[5], pType = a[6], rightPart = a[7];
-                numMatches++;
-                if (pType == '%') subst = '%';
-                else {
-                    var param = arguments[i];
-                    var pad   = '';
-                    if (pPad && pPad.substr(0,1) == "'") pad = leftpart.substr(1,1);
-                    else if (pPad) pad = pPad;
-                    var justifyRight = true;
-                    if (pJustify && pJustify === "-") justifyRight = false;
-                    var minLength = -1;
-                    if (pMinLength) minLength = parseInt(pMinLength);
-                    var precision = -1;
-                    if (pPrecision && pType == 'f') precision = parseInt(pPrecision.substring(1));
-                    var subst = param;
-                    if (pType == 'b')      subst = parseInt(param).toString(2);
-                    else if (pType == 'c') subst = String.fromCharCode(parseInt(param));
-                    else if (pType == 'd') subst = parseInt(param) ? parseInt(param) : 0;
-                    else if (pType == 'u') subst = Math.abs(param);
-                    else if (pType == 'f') subst = (precision > -1) ? Math.round(parseFloat(param) * Math.pow(10, precision)) / Math.pow(10, precision): parseFloat(param);
-                    else if (pType == 'o') subst = parseInt(param).toString(8);
-                    else if (pType == 's') subst = param;
-                    else if (pType == 'x') subst = ('' + parseInt(param).toString(16)).toLowerCase();
-                    else if (pType == 'X') subst = ('' + parseInt(param).toString(16)).toUpperCase();
-                }
-                str = leftpart + subst + rightPart;
-                i++;
+        if (typeof arguments[0] == 'object') arguments = $A(arguments[0]);
+        while (a = re.exec(str)) {
+            var leftpart   = a[1], pPad  = a[2], pJustify  = a[3], pMinLength = a[4];
+            var pPrecision = a[5], pType = a[6], rightPart = a[7];
+            numMatches++;
+            if (pType == '%') subst = '%';
+            else {
+                var param = arguments[i];
+                var pad   = '';
+                if (pPad && pPad.substr(0,1) == "'") pad = leftpart.substr(1,1);
+                else if (pPad) pad = pPad;
+                var justifyRight = true;
+                if (pJustify && pJustify === "-") justifyRight = false;
+                var minLength = -1;
+                if (pMinLength) minLength = parseInt(pMinLength);
+                var precision = -1;
+                if (pPrecision && pType == 'f') precision = parseInt(pPrecision.substring(1));
+                var subst = param;
+                if (pType == 'b')      subst = parseInt(param).toString(2);
+                else if (pType == 'c') subst = String.fromCharCode(parseInt(param));
+                else if (pType == 'd') subst = parseInt(param) ? parseInt(param) : 0;
+                else if (pType == 'u') subst = Math.abs(param);
+                else if (pType == 'f') subst = (precision > -1) ? Math.round(parseFloat(param) * Math.pow(10, precision)) / Math.pow(10, precision): parseFloat(param);
+                else if (pType == 'o') subst = parseInt(param).toString(8);
+                else if (pType == 's') subst = param;
+                else if (pType == 'x') subst = ('' + parseInt(param).toString(16)).toLowerCase();
+                else if (pType == 'X') subst = ('' + parseInt(param).toString(16)).toUpperCase();
             }
-            return str;
+            str = leftpart + subst + rightPart;
+            i++;
         }
-        });
-
+        return str;
+    };
+    $.extend({format: format, sprintf: sprintf, strConversion: conversion});
 })();
