c50995da80313e9f6fbb0064144a4a896bb68ef0
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-05-15 13:08:28 +0000

    - jsquery.strings.js: Argument.getPrecision now account wildcards (not effective yet) - code cleanup - removed sprintf temporarly because it was half implemented and untested and the code is already complicated enough

--- jquery.strings.js
+++ jquery.strings.js
@@ -32,22 +32,31 @@
   $.format('{a:u}bc', {a:'a'})     -> Abc
 */
 
-    function Argument(arg) {
+(function(){
+    Argument = function(arg) {
         this.__arg = arg;
         this.__max_precision = parseFloat('1.'+ (new Array(32)).join('1'), 10).toString().length-3;
         this.getString = function(){
             return this.__arg;
         };
-        this.getPrecision = function() {
-            var match = this.getString().match(/\.(\d+|\*)/g)
-            return match && parseInt(match[0].slice(1), 10) || this.__max_precision ;
+        this.getPrecision = function(){
+            var match = this.getString().match(/\.(\d+|\*)/g);
+            if (!match) return this.__max_precision;
+            else {
+                match = match[0].slice(1);
+                return (match == '*')? match: parseInt(match, 10);
+            }
         };
-        this.getPadding = function() {
+        this.getPadding = function(){
             var match = this.getString().match(/0(\d+|\*)/g)
             return match && parseInt(match[0].slice(1), 10) || false;
         };
+        this.getFlag = function(){
+            var match = this.getString().match(/^(0|\#|\-|\+)+/);
+            return match && match[0] || false;
+        }
     };
-(function(){
+
     //+ Jonas Raoni Soares Silva
     //@ http://jsfromhell.com/string/pad [v1.0]
     var pad = function(str, l, s, t){
@@ -160,49 +169,5 @@
         return (buffer.length > 1)? buffer.join(''): buffer[0];
     };
 
-    var sprintf = function() {
-        // This code is in the public domain. Feel free to link back to http://jan.moesen.nu/
-        // ---
-        // Changes made by Maxime Haineault (2007):
-        // - The function is now extended to Strings
-        // - The function now accept arrays as arguments, much easier to handle
-        if (!arguments || !RegExp) return;
-        var str = this;
-        var re  = /([^%]*)%('.|0|\x20)?(-)?(\d+)?(\.\d+)?(%|b|c|d|u|f|o|s|x|X)(.*)/; //'
-        var a   = b = [], i = 0, numMatches = 0;
-
-        if (typeof arguments[0] == 'object') arguments = $A(arguments[0]);
-        while (a = re.exec(str)) {
-            var leftpart   = a[1], pPad  = a[2], pJustify  = a[3], pMinLength = a[4];
-            var pPrecision = a[5], pType = a[6], rightPart = a[7];
-            numMatches++;
-            if (pType == '%') subst = '%';
-            else {
-                var param = arguments[i];
-                var pad   = '';
-                if (pPad && pPad.substr(0,1) == "'") pad = leftpart.substr(1,1);
-                else if (pPad) pad = pPad;
-                var justifyRight = true;
-                if (pJustify && pJustify === "-") justifyRight = false;
-                var minLength = -1;
-                if (pMinLength) minLength = parseInt(pMinLength);
-                var precision = -1;
-                if (pPrecision && pType == 'f') precision = parseInt(pPrecision.substring(1));
-                var subst = param;
-                if (pType == 'b')      subst = parseInt(param).toString(2);
-                else if (pType == 'c') subst = String.fromCharCode(parseInt(param));
-                else if (pType == 'd') subst = parseInt(param) ? parseInt(param) : 0;
-                else if (pType == 'u') subst = Math.abs(param);
-                else if (pType == 'f') subst = (precision > -1) ? Math.round(parseFloat(param) * Math.pow(10, precision)) / Math.pow(10, precision): parseFloat(param);
-                else if (pType == 'o') subst = parseInt(param).toString(8);
-                else if (pType == 's') subst = param;
-                else if (pType == 'x') subst = ('' + parseInt(param).toString(16)).toLowerCase();
-                else if (pType == 'X') subst = ('' + parseInt(param).toString(16)).toUpperCase();
-            }
-            str = leftpart + subst + rightPart;
-            i++;
-        }
-        return str;
-    };
-    $.extend({format: format, sprintf: sprintf, strConversion: conversion});
+    $.extend({format: format, strConversion: conversion});
 })();
