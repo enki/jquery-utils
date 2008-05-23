15c1389b40b0f79a3abcc3adc933673614b0b177
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-05-23 02:19:18 +0000

    - jquery.valid.js: seperated tests.js file - jquery.valid.js: added tests - jquery.valid.js: added namespace support - jquery.valid.js: updated demo page

--- jquery.valid.js
+++ jquery.valid.js
@@ -14,18 +14,31 @@
         // Types
         integer:  function(i, args)  { return /(^-?\d\d*$)/.test(i); },
         numeric:  function(i, args)  { return /(^-?\d\d*\.\d*$)|(^-?\d\d*$)|(^-?\.\d\d*$)/.test(i); },
-        boolean:  function(i, args)  { return /^0|1$/.test(i); },
-        alphanum: function(i, args)  { return /^[a-z0-9]+$/gi.test(i); },
-        // Date and time
-        date:     function(i, args)  { return /^\d{4}(\-|\/|\.)\d{1,2}\1\d{1,2}$/.test(i); }, // (yyyy-mm-dd || yyyy/mm/dd || yyyy.mm.dd)
-        time:     function(i, args)  { return /^([0]?[1-9]|1[0-2]):[0-5]\d(:[0-5]\d(\.\d{1,3})?)?$/.test(i); }, // (HH:MM || HH:MM:SS || HH:MM:SS.mmm)
-        // Misc
-        email:    function(i, args)  { return /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b$/.test(i); }, // http://regular-expressions.info/email.html 
+        boolean:  function(i, args)  { return (args && args.strict)? /^0|1|true|false$/.test(i): /^0|1$/.test(i); },
+        alphanum: function(i, args)  { return /^[a-zA-Z0-9.\-_]+$/.test(i); },
+        // Networking
+        net: {
+            email:    function(i, args)  { return /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum)\b$/.test(i); }, // http://regular-expressions.info/email.html 
+            ip: function(i){ return /(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)/.test(i); },
+            macaddress: function(){},
+            uri: function(i, args){ $.extend({type:'http'});}
+        },
         // MySQL
         mysql: {
-            datetime: function() {
-                return true;
-            }
+            date: function() {},
+            datetime: function() {},
+            timestamp: function() {}, // default Mysql 5+
+            time: function() {}
+        },
+        // Locals
+        canadian: {
+            date: function(i, args)  { return /^\d{4}(\-|\/|\.)\d{1,2}\1\d{1,2}$/.test(i); }, // (yyyy-mm-dd || yyyy/mm/dd || yyyy.mm.dd)
+            time: function(i, args)  { return /^([0]?[1-9]|1[0-2]):[0-5]\d(:[0-5]\d(\.\d{1,3})?)?$/.test(i); }, // (HH:MM || HH:MM:SS || HH:MM:SS.mmm)
+            currency: function() {},
+            postalCode:  function() {},
+            phoneNumber: function() {},
+            ssn: function() {},
+            sin: function() {}
         }
         
     };
