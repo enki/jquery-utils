41dbd79e06859e443be91f6122d10d3e55fcb9e9
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-05-27 00:00:49 +0000

    - jquery.cycle: changed options behaviour, added support for object as option - jquery.cycle: added interval, onCycle, onBefore, onAfter options - updated demos

--- jquery.cycle.js
+++ jquery.cycle.js
@@ -1,5 +1,5 @@
 /*
-  jQuery cycle - 0.2
+  jQuery cycle - 0.3
   http://code.google.com/p/jquery-utils/
 
   (c) Maxime Haineault <haineault@gmail.com>
@@ -9,6 +9,9 @@
 
   Changelog
   =========
+  -- 0.3 --
+  - changed options behaviour, added support for object as option
+  - added interval, onCycle, onBefore, onAfter options
   -- 0.2 --
   - changed namespace to cycle
   - now allowing custom callback functions
@@ -30,25 +33,34 @@
     }
 
     $.extend($.fn, {
-        cycle: function (imgs, delay, cb) {
+        cycle: function (imgs, options) {
             var index    = 0;
             var img      = new Image();
             var images   = [];
             var element  = null;
             var images   = shuffle(imgs)
             var element  = $(this).get(0)
-            var callback = cb || def_callback;
-
-            function cycle() {
-                callback.apply(this, [element, images[index]]);
+            var options  = options;
+            var defaults = {
+                onBefore: false,
+                onAfter:  false,
+                callback: def_callback,
+                interval: ((typeof(options)=='number')? options: 5000),
+                startOnload: true 
+            };
+            
+            var options = $.extend(defaults, options);
+            var cycle   = function() {
+                if (options.onBefore) options.onBefore.apply(this, [element, images[index]])
+                options.callback.apply(this, [element, images[index]]);
+                if (options.onAfter) options.onAfter.apply(this, [element, images[index]])
                 if (index < images.length-1) index++;
                 else index = 0;
             }
 
             for (id in images) { img.src = images[id]; } // preload
-
-            setInterval(cycle, delay);
-            cycle();
+            if (options.startOnload) cycle();
+            setInterval(cycle, options.interval);
         }
     });
 })(jQuery);
