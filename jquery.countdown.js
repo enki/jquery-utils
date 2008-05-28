f3457783b5b467c68f811c7a8d312e2cd6be3fa2
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-05-28 14:06:07 +0000

    - jquery.countdown.js: fixed bug when countdown was called on non-existent element - jquery.countdown.js: added functional argument support for years and months (ex: {year: '+1'}) - jquery.countdown.js: added unit tests - jquery.countdown.js: fixed precision bug - jquery.countdown.js: fixed months

--- jquery.countdown.js
+++ jquery.countdown.js
@@ -7,9 +7,6 @@
 
   MIT License (http://www.opensource.org/licenses/mit-license.php)
   
-  Todo
-  ====
-  - functional argument (ex: {year:'+1'})
 */
 
 (function($) {
@@ -17,9 +14,14 @@
         var o       = '';
         var timer   = false;
         var now     = new Date();
+
+        var getWeek = function(date) { 
+            var onejan = new Date(date.getFullYear(),0,1);
+            return Math.ceil((((date - onejan) / 86400000) + onejan.getDay())/7); };
+
         var options = $.extend({
-                year : now.getFullYear(), month: now.getMonth()-1, day: now.getDay(),
-                week: 0, hour: 23,  min: 59, sec: 59,
+                year : now.getFullYear(), month: now.getMonth(), day: now.getDay(),
+                week: getWeek(now), hour: now.getHours(), min: now.getMinutes(), sec: now.getSeconds(),
                 msgFormat: '%d [day|days] %hh %mm %ss', msgNow: 'Now !',
                 interval:  1000 }, options); 
 
@@ -45,14 +47,14 @@
 
             cd.days   = Math.floor(cd.remain/86400);  // days
             cd.remain = cd.remain%86400;
-            cd.hours  = Math.floor(cd.remain/3600)-1; // hours
+            cd.hours  = Math.floor(cd.remain/3600); // hours
             cd.remain = cd.remain%3600;
             cd.mins   = Math.floor(cd.remain/60);     // minutes
             cd.remain = cd.remain%60;
             cd.secs   = Math.floor(cd.remain);        // seconds
             cd.weeks  = Math.floor(cd.days/7);        // weeks 
-            cd.years  = Math.floor(cd.weeks/52);      // years
-            cd.months = Math.floor(cd.years*12);      // months
+            cd.years  = Math.floor(cd.days/365);      // years
+            cd.months = Math.floor(cd.days/30);       // months, TODO: find a more precise way to calculate months
 
             o = options.msgFormat;
             o = formatToken(o, 'y', cd.years);
@@ -65,18 +67,33 @@
 
             $(cd.el).text(o);
         };
-
+        var parse = function(type, val) {
+           function calc(str, i) {
+               if (str.slice(0,1) == '+') return i + parseInt(str.match(/\d+/)||0, 10)+1;
+               else return i - parseInt(str.match(/\d+/)||0, 10)+1;
+           }
+           if (typeof(val)=='number') return val;
+           else {
+               switch(type) {
+                   case 'year':  return calc(val, (new Date()).getFullYear());
+                   case 'month': return calc(val, (new Date()).getMonth());
+                   //case 'day':   return calc(val, (new Date()).getDate());
+                   //case 'hour':  return calc(val, (new Date()).getHours());
+                   //case 'min':   return calc(val, (new Date()).getMinutes());
+                   //case 'sec':   return calc(val, (new Date()).getSeconds());
+               }
+           }
+        };
         var cd = {
             id:     setInterval(update, options.interval), el: el,
             remain: null, el: el, days: 0, hours: 0, mins: 0, secs: 0,
             start:  function(){ return new countdown($(this.el), options); },
             stop:   function(){ return clearTimeout($(this.el).data('countdown').id)},
-            date:   new Date(options.year, options.month, options.day, 
-                             options.hour, options.min, options.sec) 
+            date:   new Date(parse('year', options.year), parse('month', options.month), parse('day', options.day), 
+                             parse('hour', options.hour), parse('min', options.min),     parse('sec', options.sec)) 
         };
         $(el).data('countdown', cd);
-
         return  update(), $(el).data('countdown');
     };
-    $.fn.countdown = function(args) { return new countdown(this.get(0), args); };
+    $.fn.countdown = function(args) { if(this.get(0)) return new countdown(this.get(0), args); };
 })(jQuery);
--- tests/tests.countdown.js
+++ tests/tests.countdown.js
@@ -4,16 +4,26 @@ $(document).ready(function(){
         var d = new Date();
         $('#fixtures').append($('<span id="countdown-1"></span>'))
                       .append($('<span id="countdown-2"></span>'))
-                      .append($('<span id="countdown-3"></span>'));
+                      .append($('<span id="countdown-3"></span>'))
+                      .append($('<span id="countdown-4"></span>'))
+                      .append($('<span id="countdown-5"></span>'));
         // begin tests
-        expect(3);
+        expect(5);
         $('#countdown-1').countdown({msgFormat: '%d days', year: d.getFullYear(), month: d.getMonth(), day: d.getDate()+2, hour:1,min:1,sec:1 });
         equals($('#countdown-1').text(), '1 days', 'Basic countdown');
         $('#countdown-2').countdown({msgFormat: '%d [day|days]', year: d.getFullYear(), month: d.getMonth(), day: d.getDate()+2, hour:1,min:1,sec:1 });
         equals($('#countdown-2').text(), '1 day', 'Plurarl handling');
         $('#countdown-3').countdown({msgNow: 'Now!', year: d.getFullYear(), month: d.getMonth(), day: d.getDate(), hour:d.getHours(),min:d.getMinutes(),sec:d.getSeconds() });
         equals($('#countdown-3').text(), 'Now!', 'Custom now message');
+        $('#countdown-4').countdown({msgFormat: '%y', year: '+1' });
+        equals($('#countdown-4').text(), '1', 'Functional argument (year)');
 
+        $('#countdown-5').countdown({msgFormat: '%M', month: '+1' });
+        equals($('#countdown-5').text(), '1', 'Functional argument (month)');
         $('#fixtures').html('');
+        
+        // $('#countdown-5').countdown({msgFormat: '%d', day: '+1' });
+        // equals($('#countdown-5').text(), '1', 'Functional argument (day)');
+        // $('#fixtures').html('');
     });
 });
