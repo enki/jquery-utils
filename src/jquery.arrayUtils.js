a49eed77523f405073fcddc3d7f9603d25cfba0f
haineault <haineault@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-12-11 20:15:16 +0000

    - jquery.arrayUtils.js: refactor - jquery.arrayUtils.js: added new array utils - jquery.arrayUtils.js: added unit tests

--- src/jquery.arrayUtils.js
+++ src/jquery.arrayUtils.js
@@ -10,55 +10,154 @@
 */
 
 (function($){
+    
+    var dummy      = function(i) { return i; };
+    var truthiness = function(i) { return !!i; };
 
-    function arr(a) {
-        this.a = a;
-        return this;
-    };
+    $.extend({
 
-    $a = function(a) { 
-        return new arr(a); 
-    };
+        all: function(object, iterator){
+            var output   = true;
+            var iterator = iterator || truthiness;
+            $.each(object, function(idx, i){
+                if (!iterator(i)) { output = false; }
+            });
+            return output;
+        },
 
-    // SUM
-    arr.prototype.sum = function() {
-        var t = 0;
-        $.each(this.a, function(){
-            t = t + parseFloat(this, 10);
-        });
-        return t;
-    };
+        any: function(object, iterator){
+            var output   = false;
+            var iterator = iterator || truthiness;
+            $.each(object, function(idx, i){
+                return iterator(i) && !(output = true) || true;
+            });
+            return output;
+        },
 
-    // MERGE
-    arr.prototype.merge = function(a2) {
-        this.a = $.merge(this.a, a2);
-        return this;
-    };
+        // equivalent of array.find
+        detect: function(object, iterator){
+            var output   = false;
+            var iterator = iterator || truthiness;
+            $.each(object, function(idx, i){
+                //return iterator(i) && !(output = i) || true;
+                if (iterator(i)) { 
+                    output = i; 
+                    return false;
+                }
+            });      
+            return output;
+        },
 
-    // PLUCK
-    // Fetching the same property for all the elements. 
-    // Returns a array of the property values.
-    arr.prototype.pluck = function(property) {
-        var rs = [];
-        $.each(this.a, function(){
-            rs.push(this[property]);
-        });
-        this.a = rs;
-        return this;
-    };
+        eachSlice: function(object, size, iterator){
+            var index = - size, slices = [];
+            while ((index += size) < object.length){ 
+                slices.push($.map(object.slice(index, index + size), iterator || dummy)); 
+            } 
+            return slices;
+        },
 
-    // ZIP
-    arr.prototype.zip = function(a2) {
-        var o = [];
-        $.each(this.a, function(idx, i){
-            if (a2[idx]) { o.push([i, a2[idx]]); }
-        });
-        this.a = o;
-        return this;
-    };
+        inject: function(object, acc, iterator){
+            $.each(object, function (idx, val) {
+                acc = iterator(acc, val, idx);
+            }); 
+            return acc;
+        },
 
-    // OUTPUT RESULT(S) 
-    arr.prototype.end = function() {
-        return this.a;
-    };
+        invoke: function(object, method, args){
+            $.each(object, function(){
+                if ($.isFunction(method)){
+                    method.apply(object, args);
+                }
+                else if ($.isFunction(window[method])){
+                    window[method].apply(object, args);
+                }
+            });
+            return object;
+        },
+
+        max: function(object, iterator) {
+            var output = false; 
+            $.each(object, function (idx, val) {
+                var rs = (iterator || dummy)(val, idx);
+                if (!output || rs > output) { output = rs; }
+            }); 
+            return output;
+        },
+
+        min: function(object, iterator) {
+            var output = false; 
+            $.each(object, function (idx, val) {
+                var rs = (iterator || dummy)(val, idx);
+                if (!output || rs < output) { output = rs; }
+            }); 
+            return output;
+        },
+
+        partition: function(object, iterator) {
+            var trues = [], falses = []; 
+            $.each(object, function (idx, val) {
+                ((iterator || truthiness)(val, idx) ? trues : falses).push(val);
+            }); 
+            return [trues, falses];
+        },
+        
+        pluck: function(object, property, iterator) {
+            var output = [];
+            var iterator = iterator || dummy;
+            $.each(object, function(){
+                output.push(iterator(this[property]));
+            });
+            return output;
+        },
+
+        reject: function(object, iterator){
+            return $.select(object, (iterator || function(i){ return !i; }));
+        },
+
+        // findAll equivalent
+        select: function(object, iterator){
+            var output   = [];
+            var iterator = iterator || truthiness;
+            $.each(object, function(idx, i){
+                if (iterator(i)) { 
+                    output.push(i); 
+                }
+            });
+            return output;
+        },
+
+        sum: function(object, iterator){
+            var iterator = iterator || function(i) { return parseInt(i, 10) };
+            var t = 0;
+            $.each(object, function(){
+                var v = iterator(this)
+                if (!isNaN(v)) { t = t + v; }
+            });
+            return t;
+        },
+
+        zip: function(object, object2, iterator) {
+            var output = [];
+            var iterator = iterator || dummy;
+            $.each(object, function(idx, i){
+                if (object2[idx]) { output.push([i, object2[idx]]); }
+            });
+            return output;
+        }
+    });
+
+    $.extend($.fn, {
+        all:       function(iterator) { return $.all(this, iterator); },
+        any:       function(iterator) { return $.any(this, iterator); },
+        pluck:     function(property, iterator) { return $.pluck(this, property, iterator); },
+        detect:    function(iterator) { return $($.detect(this, iterator)); },
+        eachSlice: function(size, iterator) { return $.eachSlice(this, size, iterator); },
+        select:    function(iterator) { return $.findAll(this, iterator); },
+        sum: function(iterator) {
+            var iterator = iterator || function(i) {
+                return parseFloat($(i).val() || $(i).text(), 10);
+            };
+            return $.sum(this, iterator);
+        }
+    });
 })(jQuery);
--- test/unit/jquery.arrayUtils.js
+++ test/unit/jquery.arrayUtils.js
@@ -1,24 +1,100 @@
 $(function(){
 
     module('jquery.arrayUtils.js');
-    test('array.merge', function() {
+
+    // ALL
+    test('$.all', function() {
+        expect(6);
+        equals($.all([]),              true,  'empty array yield true');
+        equals($.all([1,2,3]),         true,  'basic positive verification');
+        equals($.all([1,2,'']),        false, 'empty string yield false');
+        equals($.all([1,2,false]),     false, 'false yield false');
+        equals($.all([1,2,undefined]), false, 'undefined yield false');
+        equals($.all([1,2,null]),      false, 'undefined yield false');
+    });
+    test('$.fn.all', function() {
         expect(2);
-        equals($a([1,2]).merge([3,4]).end().join(''), '1234', 'Simple merge');
-        equals($a([1,2]).merge([1,2]).end().join(''), '1212', 'Merge does not remove duplicate by default');
+        ok($('<b>a</b><b>a</b>').all(function(i){ return $(i).text() == 'a'; }), 'Positive test with iterator');
+        ok(!$('<b>a</b><b>b</b>').all(function(i){ return $(i).text() == 'a'; }), 'Negative test with iterator');
+    });
+
+    // ANY
+    test('$.any', function() {
+        expect(7);
+        equals($.any([]),                     false,'empty array yield false');
+        equals($.any([false,null,undefined]), false,'test with all falsy values');
+        equals($.any([1,2,3]),                true, 'basic positive verification');
+        equals($.any([1,2,'']),               true, 'empty string yield true');
+        equals($.any([1,2,false]),            true, 'false yield true');
+        equals($.any([1,2,undefined]),        true, 'undefined yield true');
+        equals($.any([1,2,null]),             true, 'undefined yield true');
+    });
+    test('$.fn.any', function() {
+        expect(3);
+        ok($('<b>a</b><b>a</b>').all(function(i){ return $(i).text() == 'a'; }), 'Positive test with iterator');
+        ok(!$('<b>a</b><b>b</b>').all(function(i){ return $(i).text() == 'a'; }), 'Positive test (2) with iterator');
+        ok(!$('<b>c</b><b>b</b>').all(function(i){ return $(i).text() == 'a'; }), 'Negative test with iterator');
+    });
+
+    // DETECT 
+    test('$.detect', function() {
+        expect(3);
+        equals($.detect([1,2,3], function(i){return i/2==1;}), 2,'Positive detection');
+        equals($.detect([1,2,3], function(i){return i==5;}), false,'No detection');
+        equals($.detect([5,1,5], function(i){return i==5;}), 5,'Two matches');
+    });
+    test('$.fn.detect', function() {
+        expect(1);
+        ok($('<b>a</b><i>a</i>').detect(function(i){ return $(i).text() == 'a'; }).get(0).nodeName == 'B', 'detect with dom object');
+    });
+
+    // EACHSLICE
+    test('$.eachSlice', function() {
+        expect(1);
+        equals($.eachSlice([1,2,3,4,5,6,7,8,9, 10], 3, function(i){ return i+'a';})[3], '10a', 'Slicing with iterator');
+    });
+    test('$.fn.eachSlice', function() {
+        expect(2);
+        equals($('<b>A</b><b>B</b><b>C</b><b>D</b><b>E</b><b>F</b>').eachSlice(2).length, 3, 'Slicing with DOM elements');
+        equals($('<b>A</b><b>B</b><b>C</b><b>D</b><b>E</b><b>F</b>').eachSlice(2, function(i){return $(i).text().toLowerCase(); })[1][1], 'd', 'Slicing with DOM elements with iterator');
+    });
+
+    // INJECT
+    test('$.inject', function() {
+        expect(1);
+        equals($.inject([1,2,3], 0, function(acc, i){ return acc+i; }), 6, 'Simple accumulation');
+    });
+    
+    // MAX/MIN
+    test('$.max/min', function() {
+        expect(2);
+        equals($.max([5,1,2,3]), 5, 'Simple max');
+        equals($.min([5,1,2,3]), 1, 'Simple min');
+    });
+
+    // PARTITION
+    test('$.partition', function() {
+        expect(2);
+        var part = $.partition([1,2,3,4,5,6], function(i){ return i%2==0;});
+        equals(part[0].join(''), '246', 'Partition trues');
+        equals(part[1].join(''), '135', 'Partition falses');
     });
 
     test('array.pluck', function() {
         expect(2);
-        equals($a([['a','b'],['c', 'd']]).pluck(0).end().join(''), 'ac', 'pluck by index');
-        equals($a(['a','b']).pluck(0).end().join(''), 'ab', 'Invalid array return correctly')
+        equals($.pluck([['a','b'],['c', 'd']], 0).join(''), 'ac', 'pluck by index');
+        equals($.pluck(['a','b'], 0).join(''), 'ab', 'Invalid array return correctly')
+    });
+
+    test('array.select', function() {
+        expect(1);
+        equals($.select([1,2,3,4,5,6], function(i){ return i%2==0; }).join(''), '246', 'Simple select');
     });
     
     test('array.zip', function() {
         expect(3);
-        equals($a([1,2,3]).zip([1,2,3]).end().join(''), "1,12,23,3", 'basic zip');
-        equals($a([1,2,3,4]).zip([1,2,3]).end().join(''), "1,12,23,3", 'basic zip with arr1 longer');
-        equals($a([1,2,3]).zip([1,2,3,4]).end().join(''), "1,12,23,3", 'basic zip with arr2 longer');
+        equals($.zip([1,2,3], [1,2,3]).join(''), "1,12,23,3", 'basic zip');
+        equals($.zip([1,2,3,4], [1,2,3]).join(''), "1,12,23,3", 'basic zip with arr1 longer');
+        equals($.zip([1,2,3], [1,2,3,4]).join(''), "1,12,23,3", 'basic zip with arr2 longer');
     });
-
-
 });
